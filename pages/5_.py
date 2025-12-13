d1d04e4224d35564a7d9fb491d984a8df462f9b2
ABDOU <abderehmensef00@gmail.com>
2025-12-13 03:39:55 +0100

    'test'

--- pages/5_.py
+++ pages/5_.py
@@ -1,5 +1,7 @@
+import streamlit as st  # N'oubliez pas cette ligne au début !
+
 with open("extension_v1.zip", "rb") as f:
-    st.download_button(
+    st.download_button(  # <--- C'est ici que c'était écrit "set"
         label="📥 Télécharger l'Extension Chrome",
         data=f,
         file_name="predatory_detector_extension.zip",
